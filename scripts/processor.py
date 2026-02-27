4b8fb3683ae8638377d03b21e0349a2388930617
Pritesh Akhani <pritesh@kbasystems.com>
2026-02-27 11:27:08 +0530

    no message

--- scripts/processor.py
+++ scripts/processor.py
@@ -1,34 +1,45 @@
+from models.sentiment_model import SentimentModel
+from models.embedding_model import EmbeddingModel
+from models.topic_model import TopicModel
+
 from collections import defaultdict
 import numpy as np
 
-def process(self, texts):
-    sentiments = self.sentiment_model.predict_batch(texts)
-    embeddings = self.embedding_model.encode(texts)
-    topics = self.topic_model.fit_predict(embeddings)
 
-    results = []
-    cluster_data = defaultdict(list)
+class TextProcessor:
+    def __init__(self):
+        self.sentiment_model = SentimentModel()
+        self.embedding_model = EmbeddingModel()
+        self.topic_model = TopicModel(n_clusters=2)
 
-    for i, (text, sentiment) in enumerate(zip(texts, sentiments)):
-        cluster_id = int(topics[i])
+    def process(self, texts):
+        sentiments = self.sentiment_model.predict_batch(texts)
+        embeddings = self.embedding_model.encode(texts)
+        topics = self.topic_model.fit_predict(embeddings)
 
-        score = sentiment["score"]
-        label = sentiment["label"]
+        results = []
+        cluster_data = defaultdict(list)
 
-        sentiment_value = score if label == "POSITIVE" else -score
+        for i, (text, sentiment) in enumerate(zip(texts, sentiments)):
+            cluster_id = int(topics[i])
 
-        results.append({
-            "text": text,
-            "sentiment": label,
-            "confidence": score,
-            "topic_cluster": cluster_id
-        })
+            score = sentiment["score"]
+            label = sentiment["label"]
 
-        cluster_data[cluster_id].append(sentiment_value)
+            sentiment_value = score if label == "POSITIVE" else -score
 
-    print("\n📊 Cluster Summary:")
-    for cluster_id, values in cluster_data.items():
-        avg_sentiment = np.mean(values)
-        print(f"Cluster {cluster_id} → Count: {len(values)}, Avg Sentiment: {avg_sentiment:.3f}")
+            results.append({
+                "text": text,
+                "sentiment": label,
+                "confidence": score,
+                "topic_cluster": cluster_id
+            })
 
-    return results
+            cluster_data[cluster_id].append(sentiment_value)
+
+        print("\n📊 Cluster Summary:")
+        for cluster_id, values in cluster_data.items():
+            avg_sentiment = np.mean(values)
+            print(f"Cluster {cluster_id} → Count: {len(values)}, Avg Sentiment: {avg_sentiment:.3f}")
+
+        return results
