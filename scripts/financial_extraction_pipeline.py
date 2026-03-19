e73ade05d796c9c99bb94b226a2c2eaaab46af8e
Pritesh Akhani <pritesh@kbasystems.com>
2026-03-19 12:26:31 +0530

    no message

--- scripts/financial_extraction_pipeline.py
+++ scripts/financial_extraction_pipeline.py
@@ -1,7 +1,7 @@
 from scripts.financial_extraction import FinancialExtractor
 
 def main():
-
+    print("🔥 NEW FINANCIAL PIPELINE LOADED 🔥")
     extractor = FinancialExtractor()
 
     extractor.run()
